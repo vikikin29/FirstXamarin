466de192c0d79f97e6125d50f8eaef901e4d536b
vikicklin <vikicklin@gmail.com>
2017-11-21 10:39:36 +0100

    2

--- FirstXamarin/FirstXamarin/MainPage.xaml.cs
+++ FirstXamarin/FirstXamarin/MainPage.xaml.cs
@@ -14,6 +14,30 @@ namespace FirstXamarin
             InitializeComponent();
 
         }
-        
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string input = entry.Text;
+            label2.Text = input;
+
+            string first = input.Substring(0, input.Length / 2);
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            if (first.Equals(second))
+            {
+                label2.Text = input + " IS PALINDROME";
+            }
+            else
+            {
+                label2.Text = input + " IS NOT PALINDROME";
+            };
+            
+
+
+
+        }
     }
 }
